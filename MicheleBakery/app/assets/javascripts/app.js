119ead5932d0ea64462ce4e094d316474430f6bc
Sam DeGennaro <samdegennaro@Sams-MacBook-Pro.local>
2016-05-26 15:15:01 -0400

    saves email to database

--- MicheleBakery/app/assets/javascripts/app.js
+++ MicheleBakery/app/assets/javascripts/app.js
@@ -1,9 +1,29 @@
 var bakeryEmailList = angular.module("Bakery-Email-List", []);
 
-bakeryEmailList.controller("SubscribersController",[
-  "$scope","$http",function($scope, $http){
+bakeryEmailList.controller("SubscribersController",["$scope","$http",function($scope, $http){
+
     $http.get('/api/email_subscribers').then(function(response){
       $scope.email_subscribers = response.data.email_subscribers;
     })
-  }
-])
+
+    $scope.saveEmail = function(newEmail){
+      $http.post('/api/email_subscribers', newEmail).then(function(response){
+        console.log(response);
+      })
+    }
+
+    $scope.makeEmail = function(email,zip){
+      var newEmail = {
+          email_subscriber: {
+            email: email,
+            zipcode: zip
+          }
+        }
+      console.log(newEmail);
+      $scope.saveEmail(newEmail)
+      $scope.emailInput = "";
+      $scope.zipcodeInput = "";
+    }
+
+
+}])
--- MicheleBakery/app/controllers/api/email_subscribers_controller.rb
+++ MicheleBakery/app/controllers/api/email_subscribers_controller.rb
@@ -4,4 +4,21 @@ class Api::EmailSubscribersController < ApplicationController
     render json: { email_subscribers: EmailSubscriber.all}
   end
 
+  def create
+    newSubscriber = EmailSubscriber.create email_subscriber_params
+    render json: { business: newSubscriber }
+
+  end
+
+
+  def destroy
+
+  end
+
+  private
+
+  def email_subscriber_params
+    params.require(:email_subscriber).permit(:email, :zipcode)
+  end
+
 end
