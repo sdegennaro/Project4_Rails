ed0a918088c6e4cb10a8b4eff0c3ae3a380d0bdc
Sam DeGennaro <samdegennaro@Sams-MacBook-Pro.local>
2016-06-02 11:35:29 -0400

    cleans up about and contact routes

--- MicheleBakery/config/routes.rb
+++ MicheleBakery/config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
 
-  get 'contact/index'
-
-  get 'about/index'
-
   namespace :api do
     resources :email_subscribers
   end
 
   root 'welcome#index'
+
+  get '/contact/' => "contact#index", as: :contact
+  get '/about/' => "about#index", as: :about
 
 
 end
